86a4901d16e09a86fd93b90595fc8c53a54e712f
Sherklan12e <davismendoza.08@gmail.com>
2023-09-28 20:55:14 -0300

    practicandoportyj

--- pract.py
+++ pract.py
@@ -1,16 +1,44 @@
-# listas = ["Hola", "como", "Estas"]
-# lista_distintas = ["esta" , 2, "caca", "volar"]
-# esta = ("comer", "correr", 2)
-def validar(charactere):
-    estado = False
-    vocales = ["a","e","i","o", "u"]
-    if charactere in vocales:
-        print("Si es una vocal")
-        estado = True
+print("""
+  1: tarta:
+  2: mostaza:
+  3: cofe:
+  """)
+
+def tarta():
+    print(""" 
+          Tarta
+    1: lorem
+    2: lorem
+    3: lorem      
+    """)
+    poner = int(input("Ingres lo que quiere: "))
+    el = 0
+    for i in poner:
+        el = el + i 
+    return i
+def mostaza():
+    print(""" 
+          Tarta
+    1: lorem
+    2: lorem
+    3: lorem      
+           
+        """)
+def cofe():
+    print(""" 
+          Tarta
+    1: lorem
+    2: lorem
+    3: lorem   """)
+estado = True
+while estado == True:
+    poner = input("Ingrese lo que quiere: ")
+    if poner == "tarta":
+        tarta()
+    elif poner == "mostaza":
+        mostaza()
+    elif poner == "cofe":
+        cofe()
     else:
-        print("No es una vocal ")
-        estado = False
-    return estado
-    print(vocales)
-palabra = input("Escribe el caracter: ")
-validar(palabra)
+        estado == False
+    
