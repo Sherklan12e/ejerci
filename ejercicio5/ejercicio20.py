07437d3a3a3674c3663aeed02f25a7771286fa06
Sherklan <davismendoza.08@gmail.com>
2023-12-07 13:26:44 -0300

    fgd

---
+++ ejercicio5/ejercicio20.py
@@ -0,0 +1,8 @@
+# Crear un arreglo de enteros de N elementos. Verificar si los elementos se
+# encuentran en orden estrictamente creciente. Si los elementos no se encuentran
+# en orden estrictamente creciente, imprimir por pantalla la cantidad de veces que
+# se rompe ese orden.
+# Ejemplo: {1, 3, 5, 2, 4, 6, 8, 8, 9, 10}, el n-umero de veces que se rompe el
+# orden es 2.
+
+
