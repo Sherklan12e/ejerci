8a5e02a67f0e4003f321346eb4ccaa6e01ed6633
Sherklan <davismendoza.08@gmail.com>
2023-12-14 11:47:33 -0300

    erfdfg

--- ejercicio5/ejercicio20.py
+++ ejercicio5/ejercicio20.py
@@ -4,15 +4,40 @@
 # se rompe ese orden.
 # Ejemplo: {1, 3, 5, 2, 4, 6, 8, 8, 9, 10}, el n-umero de veces que se rompe el
 # orden es 2.
-while True:
-        
-    try:
-        edad  = int(input('Ingrese su edad: '))
-        print('Tu edad es : ', edad)
-        break
-        
-    except:
-        print('Ingresaste mal')
-        
-    finally:
-        print('La ejecucion a finalizado')
+def arreglo(N):
+    vector = []
+    for i in range(1,N+1):
+        numero = int(input('Ingrese el numero: '))
+        vector.append(numero)
+    
+    return vector
+
+
+
+def verificar(arreglo):
+    count = 0
+    count2 = 0
+    arreglo1 = list(arreglo)
+    
+    for i in range(len(arreglo) -1):
+        for j in range(len(arreglo) -1):
+            if arreglo[j] > arreglo[j+1]:
+                prueba = arreglo[j]
+                arreglo[j] = arreglo[j+1]
+                arreglo[j+1] = prueba
+                
+    for i in arreglo1:
+        if arreglo[i] == arreglo1[i]:
+            count = count +1
+        elif arreglo[i] != arreglo1[i]:
+            count2 = arreglo1[i]
+            if 
+            
+            
+            
+            
+            
+
+
+
+N =  n = int(input('Ingresa de cuanto va ser tu arreglo: '))
