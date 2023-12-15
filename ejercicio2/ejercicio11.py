6243ce510ff96877aa7d4739261bcf4f4771c9c1
Sherklan <davismendoza.08@gmail.com>
2023-12-15 16:16:38 -0300

    sdf

--- ejercicio2/ejercicio11.py
+++ ejercicio2/ejercicio11.py
@@ -1,8 +1,61 @@
-# Leer un número entero desde el usuario
-numero = int(input("Ingrese un número entero: "))
+def es_bisiesto(anio):
+  """
+  Devuelve True si el año es bisiesto, False en caso contrario.
 
-# Verificar si el número es el doble de un número impar
-if numero % 2 == 0 and (numero // 2) % 2 != 0:
-    print(f"{numero} es el doble de un número impar.")
-else:
-    print(f"{numero} no es el doble de un número impar.")
+  Parámetros:
+    anio: El año a comprobar.
+
+  Devuelve:
+    True si el año es bisiesto, False en caso contrario.
+  """
+
+  return (anio % 4 == 0 and anio % 100 != 0) or anio % 400 == 0
+
+def validar_fecha(dia, mes, anio):
+  """
+  Valida una fecha compuesta por el día, mes y año.
+
+  Parámetros:
+    dia: El día de la fecha.
+    mes: El mes de la fecha.
+    anio: El año de la fecha.
+
+  Devuelve:
+    None si la fecha es válida, un mensaje de error en caso contrario.
+  """
+
+  if dia < 1 or dia > 31:
+    return "El día debe estar entre 1 y 31."
+
+  if mes < 1 or mes > 12:
+    return "El mes debe estar entre 1 y 12."
+
+  if mes == 2:
+    if es_bisiesto(anio):
+      if dia > 29:
+        return "El día en febrero no puede ser mayor que 29 en un año bisiesto."
+    else:
+      if dia > 28:
+        return "El día en febrero no puede ser mayor que 28 en un año no bisiesto."
+
+  if mes in (1, 3, 5, 7, 8, 10, 12):
+    if dia > 31:
+      return "El día en este mes no puede ser mayor que 31."
+
+  if mes in (4, 6, 9, 11):
+    if dia > 30:
+      return "El día en este mes no puede ser mayor que 30."
+
+  return None
+
+
+if __name__ == "__main__":
+  dia = int(input("Introduce el día: "))
+  mes = int(input("Introduce el mes: "))
+  anio = int(input("Introduce el año: "))
+
+  mensaje_error = validar_fecha(dia, mes, anio)
+  if mensaje_error is not None:
+    print(mensaje_error)
+  else:
+    print("Fecha válida.")
---
+++ ejercicio4/ejercicio8.py
@@ -0,0 +1,14 @@
+def sumar(num1,num2):
+    return num1 + num2
+def restar(num1,num2):
+    return num1 - num2
+def dividir(num1 ,num2):
+    return num1 / num2
+def multiplicar(num1,num2):
+    return num1 * num2
+valor1 = 34
+valor2 = 34.12
+print(sumar(valor1,valor2))
+print(restar(valor1,valor2))
+print(dividir(valor1,valor2))
+print(multiplicar(valor1,valor2))
